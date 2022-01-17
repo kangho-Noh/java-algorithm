a94ae930b4c29463737b76b3a52096ba01548334
Kangho-Noh <rkdgh98@khu.ac.kr>
2022-01-17 15:59:40 +0900

    solve: p1920 BinarySearch

---
+++ src/day01/BinarySearch/p1920/Main.java
@@ -0,0 +1,43 @@
+package day01.BinarySearch.p1920;
+
+import static java.util.Arrays.*;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		//
+		System.setIn(new FileInputStream("src/day01/BinarySearch/p1920/input.txt"));
+		//
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < m; i++) {
+			int now = Integer.parseInt(st.nextToken());
+			int i1 = Arrays.binarySearch(arr, now);
+			if (i1 < 0) {
+				System.out.println(0);
+			} else {
+				System.out.println(1);
+			}
+		}
+	}
+}
