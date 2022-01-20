38b1b8ce34f3fdd98949ab566707b03708db8acc
Kangho-Noh <rkdgh98@khu.ac.kr>
2022-01-20 23:08:22 +0900

    solve: p7578 공장 indexed tree

--- src/day03/IndexedTree/p7578/Main.java
+++ src/day03/IndexedTree/p7578/Main.java
@@ -14,7 +14,8 @@ public class Main {
 	static BufferedWriter bw;
 
 	static int N, S;
-	static int A[], B[], tree[];
+	static int A[], B[];
+	static long tree[];
 
 	public static void main(String[] args) throws Exception {
 
@@ -32,7 +33,7 @@ public class Main {
 		while (S < N) {
 			S *= 2;
 		}
-		tree = new int[S * 2];
+		tree = new long[S * 2];
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
@@ -46,13 +47,12 @@ public class Main {
 
 		//A[]를 차례대로 순회
 		//먼저 query로 지금 긋는 선과 크로스 되는 선이 있는지 확인 ans+=query(1,S,1,treenode+1, 끝)
-		//그 다음 업데이트. update(treenode,1)
-		int ans = 0;
+		//그 다음 업데이트. update(treeNode,1)
+		long ans = 0;
 		for (int i = 1; i <= N; i++) {
 			int treeInd = hashB.get(A[i]);
-			int treeNode = treeInd + S - 1;
-			ans += query(1, S, 1, treeNode + 1, S * 2 - 1);
-			update(treeNode, 1);
+			ans += query(1, S, 1, treeInd + 1, S);
+			update(treeInd, 1);
 		}
 
 		bw.write(ans + "\n");
@@ -62,10 +62,28 @@ public class Main {
 	}
 
 	private static void update(int node, int value) {
+		int treeNode = node + S - 1;
 
+		while (treeNode != 0) {
+			tree[treeNode] += value;
+			treeNode /= 2;
+
+		}
 	}
 
-	private static int query(int left, int right, int node, int queryLeft, int queryRight) {
-		return 0;
+	private static long query(int left, int right, int node, int queryLeft, int queryRight) {
+		//범위에 벗어남 -> return 0
+		//범위 걸침 -> 자식 return
+		//범위 포함 -> return tree[node];
+		if (left > queryRight || right < queryLeft) {
+			return 0;
+		} else if (queryLeft <= left && queryRight >= right) {
+			return tree[node];
+		} else {
+			int mid = (left + right) / 2;
+			long leftVal = query(left, mid, node * 2, queryLeft, queryRight);
+			long rightVal = query(mid + 1, right, node * 2 + 1, queryLeft, queryRight);
+			return leftVal + rightVal;
+		}
 	}
 }
