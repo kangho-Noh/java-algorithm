5d71ddb996a2a43d91cf0e66eaee98d935dafc18
Kangho-Noh <rkdgh98@khu.ac.kr>
2022-01-24 16:18:54 +0900

    refactor: MST algorithms refactor

--- src/day06/MinimumSpanningTree/p1922/kruskal/Main.java
+++ src/day06/MinimumSpanningTree/p1922/kruskal/Main.java
@@ -3,6 +3,7 @@ package day06.MinimumSpanningTree.p1922.kruskal;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
@@ -17,12 +18,24 @@ public class Main {
 
 	static int N, M;
 	static int[] disjointSet;
+	static List<Edge> edges;
 
 	public static void main(String[] args) throws Exception {
 
 		//
 		System.setIn(new FileInputStream("src/day06/MinimumSpanningTree/p1922/input.txt"));
 		//
+		initInputData();
+		Collections.sort(edges);
+		int ans = kruskal(edges);
+
+		bw.write(ans + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static void initInputData() throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		st = new StringTokenizer(br.readLine());
@@ -35,9 +48,8 @@ public class Main {
 		for (int i = 1; i <= N; i++) {
 			disjointSet[i] = i;
 		}
-		int ans = 0;
 
-		List<Edge> edges = new ArrayList<>();
+		edges = new ArrayList<>();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
@@ -46,8 +58,10 @@ public class Main {
 			Edge edge = new Edge(a, b, c);
 			edges.add(edge);
 		}
-		Collections.sort(edges);
+	}
 
+	private static int kruskal(List<Edge> edges) {
+		int ans = 0;
 		// 가중치 오름차순으로 간선을 돌며 사이클이 만들어지지 않을 경우 추가하며 진행 O(M)
 		for (Edge edge : edges) {
 			int v1 = edge.src;
@@ -61,11 +75,7 @@ public class Main {
 				union(v1, v2);
 			}
 		}
-
-		bw.write(ans + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
+		return ans;
 	}
 
 	private static void union(int v1, int v2) {
--- src/day06/MinimumSpanningTree/p1922/prim/Main.java
+++ src/day06/MinimumSpanningTree/p1922/prim/Main.java
@@ -3,6 +3,7 @@ package day06.MinimumSpanningTree.p1922.prim;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
@@ -17,11 +18,25 @@ public class Main {
 	static int N, M;
 	static boolean[] visited;
 
+	static ArrayList<Edge>[] edges;
+	static PriorityQueue<Edge> pq;
+
 	public static void main(String[] args) throws Exception {
 
 		//
 		System.setIn(new FileInputStream("src/day06/MinimumSpanningTree/p1922/input.txt"));
 		//
+		initInputData();
+		int start = 1; //시작 정점
+		int ans = prim(start);
+
+		bw.write(ans + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static void initInputData() throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		st = new StringTokenizer(br.readLine());
@@ -29,26 +44,27 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
 		// 간선을 저장하기 위한 배열
-		ArrayList<Edge>[] edges = new ArrayList[N + 1];
+		edges = new ArrayList[N + 1];
 		for (int i = 1; i <= N; i++) {
 			edges[i] = new ArrayList<>();
 		}
 		//정점이 추가되었는지 확인하기 위한 배열
 		visited = new boolean[N + 1];
 		//프림 알고리즘 - 최소힙
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq = new PriorityQueue<>();
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			edges[a].add(new Edge(a, b, c));
-			edges[b].add(new Edge(b, a, c));
+			edges[a].add(new Edge(b, c));
+			edges[b].add(new Edge(a, c));
 		}
-		int start = 1; //시작 정점
+	}
+
+	private static int prim(int start) {
 		int ans = 0;
-		//ans = prim(start);
 		visited[start] = true;
 		pq.addAll(edges[start]);
 
@@ -64,26 +80,20 @@ public class Main {
 			//dst에 대한 정점 추가
 			pq.addAll(edges[dst]);
 		}
-
-		bw.write(ans + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
+		return ans;
 	}
 
 	private static class Edge implements Comparable<Edge> {
-		int src, dst, weight;
+		int dst, weight;
 
-		Edge(int a, int b, int c) {
-			src = a;
+		Edge(int b, int c) {
 			dst = b;
 			weight = c;
 		}
 
 		@Override
 		public int compareTo(Edge o) {
-			int comp = Integer.compare(weight, o.weight);
-			return comp;
+			return Integer.compare(weight, o.weight);
 		}
 	}
 }
