7a358b3223aa2c536559302bd2b3e4cebddf24ff
Kangho-Noh <rkdgh98@khu.ac.kr>
2022-02-06 19:52:55 +0900

    solve: p3860 벨만포드

---
+++ src/day01/Practice/Main.java
@@ -0,0 +1,30 @@
+package day01.Practice;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static StringTokenizer st;
+	static BufferedReader br;
+	static BufferedWriter bw;
+
+	static final int INF = Integer.MAX_VALUE;
+	static int n;
+
+	public static void main(String[] args) throws Exception {
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+
+		bw.write(n - 543 + "\n");
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
